e09dca6229b91cc647874edbd9d464abee41f196
Alexey_Pankov <pankov.kek@gmail.com>
2025-11-07 17:38:54 +0300

    editing gh config again))

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'vue-todo',
+  base: '/vue-todo/',
   plugins: [
     vue(),
     vueJsx(),
